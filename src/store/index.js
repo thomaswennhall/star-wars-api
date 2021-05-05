e56385fa774711204c44c47200a350c5b9908acf
thomaswennhall <70705571+thomaswennhall@users.noreply.github.com>
2021-05-05 19:33:13 +0200

    Add pagination logic in store

--- src/store/index.js
+++ src/store/index.js
@@ -6,23 +6,42 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    lastPage: 9,
     currentPage: 1,
     characters: []
   },
   getters: {
-    getPage: state => state.currentPage,
+    getCurrentPage: state => state.currentPage,
+    getLastPage: state => state.lastPage,
     getCharacters: state => state.characters
   },
   mutations: {
     setCharacters: (state, characters) => {
       state.characters = characters
+    },
+    setPage: (state, direction) => {
+      if(direction === 'next'){
+        if(state.currentPage === state.lastPage){
+          state.currentPage = 1
+        }else{
+          state.currentPage++
+        }
+      }else if(state.currentPage === 1){
+        state.currentPage = state.lastPage
+      }else{
+        state.currentPage--
+      }
     }
   },
   actions: {
-    updateCharacters: async ({commit}) => {
+    updateCharacters: async ({commit, getters}) => {
+      const page = getters.getCurrentPage
       const data = await api.getCharactersByPage(page)
       const characters = data.results
       commit('setCharacters', characters)
+    },
+    updatePage: async ({commit}, direction) => {
+      commit('setPage', direction)
     }
   },
   modules: {
