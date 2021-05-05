f2165bd6a2c51123a13cbc181889ada78d948d98
thomaswennhall <70705571+thomaswennhall@users.noreply.github.com>
2021-05-05 19:22:18 +0200

    Add store for characters and api logic for characters

---
+++ src/api/index.js
@@ -0,0 +1,19 @@
+const axios = require('axios')
+
+const BASE_URL = 'https://swapi.dev/api'
+
+const getData = async (url) => {
+  const response = await axios.get(url)
+  return response.data
+}
+
+const getCharactersByPage = async (page = 1) => {
+  const data = await getData(`${BASE_URL}/people/?page=${page}`)
+  return data
+}
+
+//TEST ---- getCharactersByPage(2).then(console.log)
+
+module.exports = {
+  getCharactersByPage
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import api from '@/api'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    currentPage: 1,
+    characters: []
+  },
+  getters: {
+    getPage: state => state.currentPage,
+    getCharacters: state => state.characters
   },
   mutations: {
+    setCharacters: (state, characters) => {
+      state.characters = characters
+    }
   },
   actions: {
+    updateCharacters: async ({commit}) => {
+      const data = await api.getCharactersByPage(page)
+      const characters = data.results
+      commit('setCharacters', characters)
+    }
   },
   modules: {
   }
