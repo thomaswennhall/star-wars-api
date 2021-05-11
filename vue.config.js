0ffda86ca72cbf978619d602997ddeddcc1ab75d
thomaswennhall <70705571+thomaswennhall@users.noreply.github.com>
2021-05-11 22:08:46 +0200

    try adding path in config file and creating deploy script

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,11 @@ module.exports = {
     loaderOptions: {
       sass: {
         prependData: `@import "@/assets/styles/_colors.scss";`,
-      },
-    },
+      }
+    }
   },
-};
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/star-wars-characters/'
+  : '/'
+
+}
